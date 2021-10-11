ffecb83d3d035b9d5622b945053c335006061f0f
hotshot07 <aroram@tcd.ie>
2021-10-11 13:22:27 +0100

    updated querier

--- querier/src/main/java/BasicQueries.java
+++ querier/src/main/java/BasicQueries.java
@@ -1,7 +1,12 @@
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.Term;
-import org.apache.lucene.search.*;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
@@ -14,30 +19,21 @@ public class BasicQueries {
   // Limit the number of search results we get
   private static int MAX_RESULTS = 20;
 
-  public static void main(String[] args) throws IOException {
-    // Open the folder that contains our search index
+  public static void main(String[] args) throws IOException, ParseException {
+
     Directory directory = FSDirectory.open(Paths.get(INDEX_DIRECTORY));
 
-    // create objects to read and search across the index
     DirectoryReader ireader = DirectoryReader.open(directory);
     IndexSearcher isearcher = new IndexSearcher(ireader);
 
-    // builder class for creating our query
-    BooleanQuery.Builder query = new BooleanQuery.Builder();
+    Analyzer analyzer = new StandardAnalyzer();
 
-    // Some words that we want to find and the field in which we expect
-    // to find them
-    Query term1 = new TermQuery(new Term("text", "fluid"));
-    //    Query term2 = new TermQuery(new Term("content", "thermodynamics"));
-    //    Query term3 = new TermQuery(new Term("content", "bill"));
+    QueryParser queryParser = new QueryParser("text", analyzer);
+    Query q =
+        queryParser.parse(
+            "what similarity laws must be obeyed when constructing aeroelastic models of heated high speed aircraft .");
 
-    // construct our query using basic boolean operations.
-    query.add(new BooleanClause(term1, BooleanClause.Occur.SHOULD)); // AND
-    //    query.add(new BooleanClause(term2, BooleanClause.Occur.MUST)); // OR
-    //    query.add(new BooleanClause(term3, BooleanClause.Occur.MUST_NOT)); // NOT
-
-    // Get the set of results from the searcher
-    ScoreDoc[] hits = isearcher.search(query.build(), MAX_RESULTS).scoreDocs;
+    ScoreDoc[] hits = isearcher.search(q, MAX_RESULTS).scoreDocs;
 
     // Print the results
     System.out.println("Documents: " + hits.length);
